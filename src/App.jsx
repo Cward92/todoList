e0f225011d589584d7fa1ee6c634b8584d8c2bb8
Chandler Ward <chandlermward92@gmail.com>
2020-10-14 17:00:13 -0400

    set initial commit for project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className='container'>
+        <div className='row d-flex justify-content-center'>
+          <h1>Todo List</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
